ce5b74165dafc3caa0fced002fb0a8acddc7c075
HorieH <33215452+HorieH@users.noreply.github.com>
2018-03-15 15:51:07 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import firebase from './firebase'
+
+const db = firebase.database()
+const ref = db.ref('test')
+
+export default class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      count: 0
+    }
+
+    ref.on('value', snapshot => this.setState(snapshot.val()))
+  }
+
+  plus = () => {
+    ref.update({
+      count: this.state.count + 1
+    })
+  }
+
+  minus = () => {
+    ref.update({
+      count: this.state.count - 1
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.count}</Text>
+        <Button title="+" onPress={this.plus} />
+        <Button title="-" onPress={this.minus} />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
